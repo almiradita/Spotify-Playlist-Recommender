504aa154ca4e4d758c0cafdf6769e27fa6892b4a
almiradita <142095969+almiradita@users.noreply.github.com>
2023-10-25 09:19:54 +0700

    Update recommender3_app.py

--- recommender3_app.py
+++ recommender3_app.py
@@ -1,75 +1,65 @@
+import os
+# Install dependencies from requirements.txt
+os.system("pip install -r requirements.txt")
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import streamlit as st
-import os
+
+
 
 # Set your Spotify API credentials as environment variables
-os.environ["SPOTIPY_CLIENT_ID"] = 'your_client_id'
-os.environ["SPOTIPY_CLIENT_SECRET"] = 'your_client_secret'
-os.environ["SPOTIPY_REDIRECT_URI"] = 'http://localhost:8080'
+os.environ["SPOTIPY_CLIENT_ID"] = '019dae7245ac49459bb07f020206a7c4'
+os.environ["SPOTIPY_CLIENT_SECRET"] = '243fa58269414cc6977815dbca6b9ef9'
+os.environ["SPOTIPY_REDIRECT_URI"] = 'http://localhost/3630'
 
-# Initialize Spotipy with your authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-public", open_browser=False))
+# Initialize Spotipy with authentication
+auth_manager = SpotifyOAuth(scope="playlist-modify-public user-library-read user-read-recently-played user-top-read",
+                             open_browser=False)
 
-def get_artist_id(artist_name):
-    # Search for the artist
-    results = sp.search(q=artist_name, type='artist', limit=1)
-    
-    # Check if any artists were found
-    if results['artists']['total'] == 0:
-        print(f"No artist found for '{artist_name}'.")
-        return None
-    
-    # Get the ID of the first artist found
-    artist_id = results['artists']['items'][0]['id']
-    return artist_id
+spotify = spotipy.Spotify(auth_manager=auth_manager)
 
-
-# Streamlit app title and input fields
 st.title("SpotiMix: Your Playlist Maker")
-
 st.markdown(
-    """
-    Welcome to the Spotify Playlist Recommendation System! 🎵
+        """
+            Welcome to the Spotify Playlist Recommendation System! 🎵
 
-    This app is your gateway to creating personalized Spotify playlists 
-    based on your favorite artists. Whether you have a particular artist in mind
-    or are looking for recommendations, we've got you covered.
+            This app is your gateway to creating personalized Spotify playlists based on your favorite artists. Whether you have a particular artist in mind or are looking for recommendations, we've got you covered.
 
-    To get started, simply enter an artist's name in the input field below and explore
-    a world of musical possibilities. We'll provide recommendations and let you
-    craft the perfect playlist to match your mood.
+            To get started, simply enter an artist's name in the input field below and explore a world of musical possibilities. We'll provide recommendations and let you craft the perfect playlist to match your mood.
 
-    Let the music adventure begin! 🎶
-    """
-)
+            Let the music adventure begin! 🎶
+        """
+    )
 
-# Create a text input field for the artist's name
-artist_name = st.text_input("Enter an artist's name:")
 
-# Numeric input for the number of tracks to add
-num_tracks_to_add = st.number_input("Number of tracks to add", min_value=1, max_value=50, value=10)
+# User selects their preferred artist
+selected_artist = st.text_input("Enter the name of the artist:")
 
-# Input field for the playlist name
-playlist_name = st.text_input("Enter a playlist name:", "Recommended Playlist")  # Default name
+# Search for tracks by the selected artist
+if selected_artist:
+    artist_tracks = spotify.search(q=f"artist:{selected_artist}", type="track", limit=10)
 
-if st.button("Create Playlist") and artist_name:
-    # Get artist ID based on the artist's name
-    artist_id = get_artist_id(artist_name)
-    if artist_id:
-        # Create a playlist and get playlist ID
-        playlist_id, playlist = create_playlist(playlist_name)
+    if artist_tracks:
 
-        # Get recommended tracks based on the selected artist
-        recommendations = get_recommendations_by_artist(artist_id)
+        # Create a playlist with the tracks by the selected artist
+        playlist_name = f"{selected_artist} Playlist"
+        user_id = spotify.me()["id"]
+        playlist = spotify.user_playlist_create(user=user_id, name=playlist_name, public=True)
 
-        # Extract track URIs from recommendations
-        track_uris = [track['uri'] for track in recommendations]
+        # Add the artist's tracks to the playlist
+        track_uris = [track["uri"] for track in artist_tracks["tracks"]["items"]]
+        spotify.playlist_add_items(playlist_id=playlist["id"], items=track_uris)
 
-        # Limit the number of tracks to add based on user input
-        track_uris = track_uris[:num_tracks_to_add]
+        # Generate a Spotify link for the created playlist
+        access_token = auth_manager.get_access_token()['access_token']
+        spotify_link = f"https://open.spotify.com/playlist/{playlist['id']}?si={access_token}"
 
-        # Add recommended tracks to the playlist
-        add_tracks_to_playlist(playlist_id, track_uris)
+        st.success(f"Playlist '{playlist_name}' created with {len(track_uris)} tracks by {selected_artist}.")
+        st.markdown(f"Access your playlist on [Spotify]({spotify_link}).")
+    else:
+        st.warning(f"No tracks found for the artist {selected_artist}. Please try a different artist.")
+else:
+    st.warning("Enter the name of an artist to create a playlist.")
 
-        st.success(f"Playlist '{playlist_name}' created with {len(track_uris)} recommended tracks.")
+
